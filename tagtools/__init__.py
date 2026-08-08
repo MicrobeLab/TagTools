0e08ec57d3d68b8e6c2608e5aaa2a54a145f306b
MicrobeLab <52490989+MicrobeLab@users.noreply.github.com>
2026-08-08 14:51:51 +0800

    Update __init__.py

--- tagtools/__init__.py
+++ tagtools/__init__.py
@@ -6,10 +6,13 @@ Modules:
     cluster: Cluster prediction from SNP data (cluster.py)
     impute: Imputed haplotype using groups of cells (impute.py)
     demultiplex: Demultiplexing using demuxlet (demultiplex.py)
+    index: Create index for bgzip compressed file (index.py)
+    split: Split BAM file by tag (split.py)
+    call: Call variants and perform genotyping (call.py)
 """
 
 __version__ = "1.0.0"
 __author__ = "Guo-wang Lin"
 __email__ = "lingw6@alumni.sysu.edu.cn"
 
-from . import reference, cluster, impute, demultiplex
+from . import reference, cluster, impute, demultiplex, index, split, call
